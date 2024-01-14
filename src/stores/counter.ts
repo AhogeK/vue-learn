956134767bb24b108dea53e5b111319b56bfdeee
AhogeK <ahogek@gmail.com>
2024-01-14 22:28:23 +0800

    style: make the code a bit more elegant

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,10 +1,16 @@
-import { ref, computed } from 'vue'
+import { ref, computed, type Ref, type ComputedRef } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
+interface CounterStore {
+  count: Ref<number>
+  doubleCount: ComputedRef<number>
+  increment: () => void
+}
+
+export const useCounterStore = defineStore('counter', (): CounterStore => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
-  function increment() {
+  function increment(): void {
     count.value++
   }
 
